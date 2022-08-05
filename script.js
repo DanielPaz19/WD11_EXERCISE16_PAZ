bd3aecbaf5355f5a2f75d76f5672a57c3af6f793
DanielPaz19 <anyepaz1019@gmail.com>
2022-08-06 00:31:47 +0800

    update

---
+++ script.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const inputTemp = document.querySelector("#inputTemp");
+const outputTemp = document.querySelector("#outputTemp");
+const selectInputTemp = document.querySelector("#selectInputTemp");
+const selectOutputTemp = document.querySelector("#selectOutputTemp");
+
+const convert = (input) => {
+  const selectedInput = selectInputTemp.value;
+  const selectedOutput = selectOutputTemp.value;
+
+  // Celcius
+  if (selectedInput == 1) {
+    if (selectedOutput == 1) {
+      outputTemp.value = input;
+    }
+    if (selectedOutput == 2) {
+      outputTemp.value = celciusToKelvin(input);
+    }
+    if (selectedOutput == 3) {
+      outputTemp.value = celciusToFahrenheit(input);
+    }
+  }
+
+  // Kelvin
+  if (selectedInput == 2) {
+    if (selectedOutput == 1) {
+      outputTemp.value = celciusToKelvin(input, true);
+    }
+    if (selectedOutput == 2) {
+      outputTemp.value = input;
+    }
+    if (selectedOutput == 3) {
+      outputTemp.value = kelvinToFahrenheit(input);
+    }
+  }
+
+  // Fahrenheit
+  if (selectedInput == 3) {
+    if (selectedOutput == 1) {
+      outputTemp.value = celciusToFahrenheit(input, true);
+    }
+    if (selectedOutput == 2) {
+      outputTemp.value = kelvinToFahrenheit(input, true);
+    }
+    if (selectedOutput == 3) {
+      outputTemp.value = input;
+    }
+  }
+};
+
+const celciusToKelvin = (value, reverse = null) =>
+  reverse ? Number(value) * -272.15 : Number(value) * 274.15;
+
+const celciusToFahrenheit = (value, reverse = null) =>
+  reverse ? Number(value) * -17.222222222 : Number(value) * 33.8;
+
+const kelvinToFahrenheit = (value, reverse = null) =>
+  reverse ? Number(value) * 255.92777778 : Number(value) * -457.87;
+
+// Event while typing on the input box
+inputTemp.addEventListener("keyup", function (e) {
+  convert(e.target.value);
+});
+
+// Event when select changes
+selectInputTemp.addEventListener("change", function () {
+  convert(inputTemp.value);
+});
+
+selectOutputTemp.addEventListener("change", function () {
+  convert(inputTemp.value);
+});
